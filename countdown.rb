89e17a27df4fece4ca19312e96c7d52ecd1bb18b
Justice Payne <spikepayne@yahoo.com>
2020-08-25 03:50:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,18 @@
 def countdown (num)
-  while num > 0
+    while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1
-  end
+    end
 "HAPPY NEW YEAR!"
 end
 
 
 def countdown_with_sleep(num)
-  while num > 0
+    while num > 0
     puts "#{num} SECOND(S)!"
     sleep (1)
     num -=
+    end
 
   "HAPPY NEW YEAR!"
 end
